87c201da66962fa6f58458be2935639d9e1dde89
deven9852 <deven9852@gmail.com>
2019-05-29 10:45:25 +0530

    Color coded widgets.

--- lib/pages/select_widget_dialog.dart
+++ lib/pages/select_widget_dialog.dart
@@ -7,6 +7,14 @@ class SelectWidgetDialog extends StatefulWidget {
 }
 
 class _SelectWidgetDialogState extends State<SelectWidgetDialog> {
+  var colorCombinations = [
+    ColorPair(Colors.blue, Colors.blue[50]),
+    ColorPair(Colors.pink, Colors.pink[50]),
+    ColorPair(Colors.deepPurple, Colors.deepPurple[50]),
+    ColorPair(Colors.orange, Colors.orange[50]),
+    ColorPair(Colors.green, Colors.green[50]),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,22 +25,23 @@ class _SelectWidgetDialogState extends State<SelectWidgetDialog> {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, childAspectRatio: 2.0),
         itemBuilder: (context, position) {
+          ColorPair pair = _getColorPair(getNewModelFromType(WidgetType.values[position]));
+
           return Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.all(4.0),
             child: Card(
-              color: Colors.blue[50],
+              color: pair.backgroundColor,
               child: InkWell(
                 onTap: () {
-                  Navigator.pop(
-                      context, getNewModelFromType(WidgetType.values[position]));
+                  Navigator.pop(context,
+                      getNewModelFromType(WidgetType.values[position]));
                 },
                 child: Center(
                   child: Text(
                     WidgetType.values[position].toString().split(".")[1],
                     style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500
-                    ),
+                        color: pair.textColor,
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
@@ -43,4 +52,26 @@ class _SelectWidgetDialogState extends State<SelectWidgetDialog> {
       ),
     );
   }
+
+  ColorPair _getColorPair(ModelWidget widget) {
+    switch(widget.nodeType) {
+
+      case NodeType.SingleChild:
+        return colorCombinations[4];
+        break;
+      case NodeType.MultipleChildren:
+        return colorCombinations[1];
+        break;
+      case NodeType.End:
+        return colorCombinations[0];
+        break;
+    }
+  }
+}
+
+class ColorPair {
+  Color textColor;
+  Color backgroundColor;
+
+  ColorPair(this.textColor, this.backgroundColor);
 }
