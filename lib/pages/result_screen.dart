d8fdfb49054ac5a3b3f58b6bb9cf63461f8739cb
deven9852 <deven9852@gmail.com>
2019-05-29 12:24:20 +0530

    Added cancel button

--- lib/pages/result_screen.dart
+++ lib/pages/result_screen.dart
@@ -12,8 +12,23 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: widget.widget,
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          body: widget.widget,
+        ),
+        SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
+            child: GestureDetector(
+              child: Icon(Icons.clear, size: 30.0,),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
