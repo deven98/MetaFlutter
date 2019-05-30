02cda995a1cb507e4643036a1bceb37e61229af0
deven9852 <deven9852@gmail.com>
2019-05-30 14:43:19 +0530

    Added search functionality in select widgets

--- lib/pages/select_widget_dialog.dart
+++ lib/pages/select_widget_dialog.dart
@@ -8,47 +8,96 @@ class SelectWidgetDialog extends StatefulWidget {
 }
 
 class _SelectWidgetDialogState extends State<SelectWidgetDialog> {
+  TextEditingController _searchController = TextEditingController();
+
+  String searchParam = "";
 
   @override
   Widget build(BuildContext context) {
+    List<WidgetType> types = [];
+
+    if (searchParam.trim() == "") {
+      types = WidgetType.values;
+    } else {
+      WidgetType.values.forEach((type) {
+        if(type.toString().toLowerCase().contains(searchParam.toLowerCase())) {
+          types.add(type);
+        }
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Select Widget"),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Icon(Icons.search),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _buildSearchBar(),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, childAspectRatio: 2.0),
+            itemBuilder: (context, position) {
+              ColorPair pair = getColorPair(
+                  getNewModelFromType(WidgetType.values[WidgetType.values.indexOf(types[position])]));
+
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Card(
+                  color: pair.backgroundColor,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context,
+                          getNewModelFromType(WidgetType.values[WidgetType.values.indexOf(types[position])]));
+                    },
+                    child: Center(
+                      child: Text(
+                        types[position].toString().split(".")[1],
+                        style: TextStyle(
+                            color: pair.textColor,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: types.length,
           ),
         ],
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 2.0),
-        itemBuilder: (context, position) {
-          ColorPair pair = getColorPair(getNewModelFromType(WidgetType.values[position]));
+    );
+  }
 
-          return Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Card(
-              color: pair.backgroundColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.pop(context,
-                      getNewModelFromType(WidgetType.values[position]));
-                },
-                child: Center(
-                  child: Text(
-                    WidgetType.values[position].toString().split(".")[1],
-                    style: TextStyle(
-                        color: pair.textColor,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
+  Widget _buildSearchBar() {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      height: 60.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Search"),
               ),
             ),
-          );
-        },
-        itemCount: WidgetType.values.length,
+            flex: 4,
+          ),
+          Expanded(
+            child: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  searchParam = _searchController.text.trim();
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
