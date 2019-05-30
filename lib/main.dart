69e66cfb7883f6ee2a3000bfe39b6dc82ded8c38
deven9852 <deven9852@gmail.com>
2019-05-30 16:14:17 +0530

    Added logo for android

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MetaFlutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         accentColor: Colors.blueAccent,
